38aad79ddf6b5c057db06b6a939714aee8c7b0d9
cephastsai <petertsai1994@gmail.com>
2016-12-12 18:01:10 +0800

    階段UI部分檔案

---
+++ Unfortune_Card/Assets/Script/BattleUI/LightSelfDestroy.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LightSelfDestroy : MonoBehaviour {
+
+	private int timer_i = 0;
+	private bool start_timer3 = true;
+
+	IEnumerator timer3()
+	{
+		yield return new WaitForSeconds (1);
+		timer_i++;
+		start_timer3 = true;
+	}
+
+	// Update is called once per frame
+	void Update () {
+		if (start_timer3) 
+		{
+			StartCoroutine ("timer3");
+			start_timer3 = false;
+		}
+		if (timer_i > 2) 
+		{
+			Destroy(gameObject);
+		}
+	}
+}
---
+++ Unfortune_Card/Assets/Script/BattleUI/TurnEndStart.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TurnEndStart : MonoBehaviour {
+
+	public bool In = false;
+	public float x;
+	public float y;
+
+	void Start()
+	{
+		x = transform.localScale.x;
+		y = transform.localScale.y;
+	}
+	// Update is called once per frame
+	void Update () 
+	{
+		if (In == true && x > 70) 
+		{
+			transform.localScale = new Vector3 (x, y, 100);
+			x -= 3;
+			y -= 3;
+		}
+		if (In == false && x < 100) 
+		{
+			transform.localScale = new Vector3 (x, y, 100);
+			x += 3;
+			y += 3;
+		}
+	}
+
+	public void StartGetIn()
+	{
+		In = true;
+		x = transform.localScale.x;
+		y = transform.localScale.y;
+	}
+
+	public void StartGrtOut()
+	{
+		x = transform.localScale.x;
+		y = transform.localScale.y;
+		In = false;
+	}
+}
